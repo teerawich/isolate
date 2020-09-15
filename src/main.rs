030761dd7464b2b81a5fcb1a66dd8358d70ac186
Teerawich Phasitthanakamol <p.teerawich@hotmail.com>
2020-09-15 17:10:00 +0700

    [Edit] setup ticket, branch and project module to main file.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use actix_web::{ HttpServer, App};
+use actix_web::{ HttpServer, web, App};
 
 mod application;
+mod branch;
+mod project;
+mod ticket;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -10,6 +13,25 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(||{
         App::new()
+          .service(
+            web::scope("/projects")
+              .service(project::project_list)
+              .service(project::create_project)
+              .service(project::remove_project)
+
+          )
+          .service(
+            web::scope("/branches")
+              .service(branch::branch_list)
+              .service(branch::create_branch)
+              .service(branch::remove_branch)
+          )
+          .service(
+            web::scope("/tickets")
+              .service(ticket::ticket_list)
+              .service(ticket::create_ticket())
+              .service(ticket::remove_ticket())
+          )
           .service(application::ping)
           .service(application::ping_id)
     })
