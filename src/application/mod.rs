075196e2ff65b7d167fca0abe63817aed4a09bed
Teerawich Phasitthanakamol <p.teerawich@hotmail.com>
2020-09-15 13:40:43 +0700

    [Add] json library to project.

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -1,5 +1,12 @@
 use actix_web::{HttpResponse, web};
 use actix_web_codegen::get;
+use actix_web::web::Json;
+use serde::{Serialize};
+
+#[derive(Serialize, Debug)]
+pub struct Client {
+  user_id: String,
+}
 
 #[get("/ping")]
 pub async fn ping() -> Result<HttpResponse, actix_web::Error> {
@@ -7,6 +14,6 @@ pub async fn ping() -> Result<HttpResponse, actix_web::Error> {
 }
 
 #[get("/ping/{id}")]
-pub async fn ping_id(web::Path(id) : web::Path<u32>) -> Result<HttpResponse, actix_web::Error> {
-  Ok(HttpResponse::Ok().body(format!("Hello {}!", id)))
+pub async fn ping_id(web::Path(id) : web::Path<String>) -> Result<Json<Client>, actix_web::Error> {
+  Ok(Json(Client{user_id: id}))
 }
